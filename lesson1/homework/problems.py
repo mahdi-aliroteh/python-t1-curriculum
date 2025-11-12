90513a4af45227231efb654afdd54ae7677a6f0c
mahdi-aliroteh <mahdi.aliroteh@gmail.com>
2025-11-12 01:17:01 +0000

    git commit --amend

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,32 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
+x=64
+print(x)
 
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
+a=10
+print(a)
 
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+y="strawberries"
+print(y)
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
+n="USA"
+n2="Kirkland"
+print(n)
+print(n2)
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+p="Nibbles"
+p2="3"
+p3="bunny"
+print(p,p2,p3)
